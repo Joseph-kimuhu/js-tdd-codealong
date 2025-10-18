cccaa10e553623503aaece03c6d3788095d3082b
Joseph-Kimuhu <joseph.kimuhu@student.moringaschool.com>
2025-10-18 16:51:07 +0300

    first commit

--- src/__tests__/utils.test.js
+++ src/__tests__/utils.test.js
@@ -1,9 +1,28 @@
+
+
 import { currentAgeForBirthYear } from "../utils";
 
 describe("currentAgeForBirthYear", () => {
   it("returns the age of a person based on the year of birth", () => {
+    
     const birthYear = 1984;
+    const currentYear = new Date().getFullYear();
+    const expectedAge = currentYear - birthYear;
+
+    
     const ageOfPerson = currentAgeForBirthYear(birthYear);
-    expect(ageOfPerson).toBe(38);
+
+  
+    expect(ageOfPerson).toBe(expectedAge);
+  });
+
+  it("returns 0 for someone born this year", () => {
+    const birthYear = new Date().getFullYear();
+    expect(currentAgeForBirthYear(birthYear)).toBe(0);
+  });
+
+  it("returns correct age for someone born in year 0", () => {
+    const currentYear = new Date().getFullYear();
+    expect(currentAgeForBirthYear(0)).toBe(currentYear);
   });
 });
--- src/utils.js
+++ src/utils.js
@@ -1,1 +1,8 @@
 // Your code here
+// src/utils.js
+
+export function currentAgeForBirthYear(birthYear) {
+  const currentYear = new Date().getFullYear();
+  return currentYear - birthYear;
+}
+
